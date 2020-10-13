f4527f6de36472f5ccc2799077de5e4da2c7d7dd
Minel SAYGISEVER <37018614+minelsaygisever@users.noreply.github.com>
2020-10-13 09:34:54 +0300

    Project uploaded.

---
+++ SwapWithoutThirdVariable/SwapWithoutThirdVariable/Program.cs
@@ -0,0 +1,23 @@
+﻿// This program replaces the values of two numeric variables without using a third variable.
+// Minel SAYGISEVER
+
+using System;
+
+namespace SwapWithoutThirdVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+
+            b = a + b; // b = 3
+            a = b - a; // a = 2
+            b = b - a; // b = 1
+
+            Console.WriteLine("The value of the 1st variable: {0}", a);
+            Console.WriteLine("The value of the 2nd variable: {0}", b);
+        }
+    }
+}
